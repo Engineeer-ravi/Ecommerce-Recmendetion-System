3fdf768c7bb1c010225e081b96566f5b18d538c5
Ravi  Kumar Pathak <163415843+Engineeer-ravi@users.noreply.github.com>
2025-11-14 13:34:59 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install dependencies (Render uses pip3)
-pip3 install gunicorn
-
-# Run Flask app with gunicorn
-exec gunicorn app:app --workers 3 --bind 0.0.0.0:$PORT --timeout 120
